8d536a28bdc260bc37ffb74de866d46aa3fa0aaa
n9125 <nono99112255@gmail.com>
2026-04-22 10:55:32 +1000

    initial definition of text output components

--- src/main/java/com/formcoach/textoutputgen.java
+++ src/main/java/com/formcoach/textoutputgen.java
@@ -1,4 +1,22 @@
 package com.formcoach;
 
 public class textoutputgen {
+    // text output composite parts
+    private Boolean disableFlavourText;
+    private String VBadText;
+    private String BadText;
+    private String GoodText;
+    private String PerfectText;
+    private String BadOutputFormat;
+    private String GoodOutputFormat;
+    private String FlavourTextPos1 = "Good job!";
+    private String FlavourTextPos2 = "Keep it up!";
+    private String FlavourTextPos3 = "Great work!";
+    private String FlavourTextNeg1 = "Keep on improving!";
+    private String FlavourTextNeg2 = "Don't give up!";
+
+    // eventually going to be a method that takes the coordinate inputs and gets text outputs
+    public String output() { // will take two inputs, need to check type
+        return "";
+    }
 }
