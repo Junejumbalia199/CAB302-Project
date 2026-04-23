07f3b9dd813524c5ad817874360f5bd61c4c6361
NessyWessy <nicrutledge078@gmail.com>
2026-04-23 18:53:18 +1000

    main code for exercise selection & login, it has been commented out so to test it just remove the comments around said page and comment out the others

--- src/main/java/com/formcoach/Main.java
+++ src/main/java/com/formcoach/Main.java
@@ -1,5 +1,7 @@
 package com.formcoach;
 
+import com.formcoach.selection.ExerciseSelectionPage;
+import com.formcoach.auth.AuthPage;
 import com.formcoach.landingpage.landingpage;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -28,3 +30,44 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+//login page run code, once the db is implemented remove and switch "null"
+/*public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        AuthPage authPage = new AuthPage(
+                stage,
+                () -> System.out.println("Back pressed"),
+                () -> System.out.println("Auth successful"),
+                null
+        );
+
+        authPage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+ */
+
+//exercise selection page code, needs to be linked with the tutorial page
+/*public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        ExerciseSelectionPage selectionPage = new ExerciseSelectionPage(
+                stage,
+                () -> System.out.println("Back pressed")
+        );
+
+        selectionPage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+ */
